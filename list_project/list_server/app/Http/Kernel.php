611582ab4bc6c4c828d7f98ccac81ab887af8166
Fabio Silva <fabiodasilva95@outlook.com>
2019-10-10 21:58:28 -0300

    correção do cors e criação do front em react.js

--- list_project/list_server/app/Http/Kernel.php
+++ list_project/list_server/app/Http/Kernel.php
@@ -39,6 +39,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
+            'cors' => \App\Http\Middleware\Cors::class,
             'bindings',
         ],
     ];
@@ -61,6 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        
     ];
 
     /**
